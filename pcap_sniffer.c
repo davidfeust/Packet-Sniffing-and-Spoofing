87a0a885955873d7cbf8a460af50eb10d77a9bd7
david <davidhodefi0@gmail.com>
2021-02-23 22:45:19 +0200

    pcap_sniffer

--- pcap_sniffer.c
+++ pcap_sniffer.c
@@ -1,34 +1,53 @@
-#include <pcap.h>
 #include <stdio.h>
 #include <pcap/pcap.h>
+#include "packet_heder.h"
+#include <netinet/in.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 
-/* This function will be invoked by pcap for each captured packet.
-We can process each packet inside the function.
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+    ethernet_h *ethernetH = (ethernet_h *) (packet);
+    ip_h *ipH = (ip_h *) (packet + SIZE_ETHERNET);
+    unsigned int size_ip = IP_HL(ipH) * 4;
+    if (size_ip < 20) {
+        printf("\t*Invalid IP header length: %u bytes\n\n", size_ip);
+        return;
+    }
+
+    char src[INET_ADDRSTRLEN];
+    char dst[INET_ADDRSTRLEN];
+    printf("**Got Packet**\n");
+    inet_ntop(AF_INET, &(ipH->ip_src.s_addr), src, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(ipH->ip_dst.s_addr), dst, INET_ADDRSTRLEN);
+    printf("host: %s \ndest: %s\n\n", src, dst);
+
+/*
+    tcp_h *tcpH = (tcp_h*)(packet + SIZE_ETHERNET + size_ip);
+    unsigned int size_tcp = TH_OFF(tcpH)*4;
+    if (size_tcp < 20) {
+
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    unsigned char *payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
 */
-void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                const u_char *packet)
-{
-    printf("Got a packet\n");
 }
-int main()
-{
+
+int main() {
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
 
     struct bpf_program fp;
-    char filter_exp[] = "ip proto icmp";
-    bpf_u_int32 net;
-// Step 1: Open live pcap session on NIC with name eth3
-// Students needs to change "eth3" to the name
-// found on their own machines (using ifconfig).
-    handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf);
-// Step 2: Compile filter_exp into BPF psuedo-code
-    pcap_compile(handle, &fp, filter_exp, 0, net);
-    pcap_setfilter(handle, &fp);
-// Step 3: Capture packets
+    char filter_exp[] = "IPPROTO_ICMP"; //"ip proto icmp";
+    bpf_u_int32 net = 0;
+
+// Open live pcap session on NIC with name eth3
+    handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
+
+// Capture packets
     pcap_loop(handle, -1, got_packet, NULL);
     pcap_close(handle); //Close the handle
     return 0;
 }
-// Note: don’t forget to add "-lpcap" to the compilation command.
-// For example: gcc -o sniff sniff.c -lpcap
